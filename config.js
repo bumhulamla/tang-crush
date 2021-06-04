c2a25e0697de012e0ddcfc4d27c287e630b4ee0d
bumhulamla <85362565+bumhulamla@users.noreply.github.com>
2021-06-05 01:09:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introDesc: `Đã lỡ yêu em nhiều rồi thì anh chỉ biết ngắm mưa
+  Nhìn qua hàng cây được bao nhiêu hạt mưa
+     Là trong anh được bấy nhiêu nỗi nhớ em`,
+    btnIntro: 'ấn vào đi',
+    title: 'Quay lại với toi chứ?',
+    desc: 'Toi chỉ dám thể hiện tc với chú qua sở thích của tôi hi vọng chú thích',
+    btnYes: 'có <3',
+    btnNo: 'Không',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Toi biết mà 🥰. Yêu babi nhiều nhiều 😘😘',
+    messDesc: 'nhớ babu nhiềuuu.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/bumhulam/' 
 }
